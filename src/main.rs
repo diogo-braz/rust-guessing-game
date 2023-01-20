74452ed8c56503c554c09613f73d6233d97c4a23
Diogo Braz <diogo.braz04@gmail.com>
2023-01-20 00:38:45 -0300

    generate random number between 1 and 100

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use rand::Rng;
 use std::io;
 use std::io::Write;
 
 fn main() {
     println!("Guess the number!");
+
+    let number = rand::thread_rng().gen_range(1..=100);
+    println!("{number}"); // only for test
+
     print!("Insert your guess: ");
     io::stdout().flush().unwrap();
 
